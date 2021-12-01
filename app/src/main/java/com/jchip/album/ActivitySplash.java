10621926a9a7febd349e9c9a7306c20f5f2191f9
jiangchang <jiangchang@gmail.com>
2021-11-30 22:56:16 -0500

    slip photos work

--- app/src/main/java/com/jchip/album/ActivitySplash.java
+++ app/src/main/java/com/jchip/album/ActivitySplash.java
@@ -8,13 +8,8 @@ public class ActivitySplash extends AbstractActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.openLayer(PHOTO_LAYER);
-       // this.openLayer(ALBUM_LAYER);
+
         this.startActivity(ActivityAlbum.class);
         this.finish();
-    }
-
-    @Override
-    public void initContentView() {
     }
 }
