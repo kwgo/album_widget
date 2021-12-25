311220d743e88d0b3f0637293b4331b4e958f7df
jiangchang <jiangchang@gmail.com>
2021-12-25 14:02:32 -0500

    fix delete album bug

--- app/src/main/java/com/jchip/album/activity/AlbumActivity.java
+++ app/src/main/java/com/jchip/album/activity/AlbumActivity.java
@@ -100,7 +100,7 @@ public class AlbumActivity extends PhotoActivity {
                         removeAlbum();
                     });
                 } else {
-                    deleteAlbum();
+                    removeAlbum();
                 }
             });
         }
